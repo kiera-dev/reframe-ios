7aacf7a658171bf8db3bbb7ffbe4fabd81648e67
Kiera Castner <kieracastner@gmail.com>
2026-02-18 12:47:39 -0500

    Initial Commit

---
+++ ReFrame/ReFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReFrameApp.swift
+//  ReFrame
+//
+//  Created by Kiera Castner on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ReFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
